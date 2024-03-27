35d7c29158ec5196a697976e015079d285d43916
Christopher Wróbel <christopher.wrobel@tum.de>
2024-03-27 12:27:00 +0100

    Add exit handler to make cursor visible again

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,26 @@
 #include "simulation.h"
 #include <iostream>
 #include <string>
+#include <csignal>
 #include <thread>
 
+void exit_handler(int s) {
+    // move curser down 12 lines
+    std::cout << "\033[12B" << std::endl;
+    // make cursor visible again
+    std::cout << "\033[?25h" << std::endl;
+    std::cout << "Exiting..." << std::endl;
+    exit(1);
+}
+
 int main(int argc, char **argv) {
+    // register exit handler
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = exit_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
     // BUG: forget to check if input file is provided
     std::basic_string<char> settings_path = std::basic_string<char>(argv[1]);
     std::cout << "Settings path: " << settings_path << std::endl;
@@ -17,6 +34,8 @@ int main(int argc, char **argv) {
     // initialize simulation
     // TODO: pass simulation parameters and initial conditions
     Simulation sim = Simulation();
+
+    std::cout << "Starting simulation, press Ctrl-C / Strg-C to exit" << std::endl;
 
     // loop
     for (;;) {
